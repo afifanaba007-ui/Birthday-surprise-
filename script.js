73bebfdfcac255e940e293955347b7be9296d01a
afifanaba007-ui <afifanaba007@gmail.com>
2026-08-06 01:55:04 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 const photos = [
-  "images/1000048755.jpg",
-  "images/1000053593.webp",
-  "images/1743531221047.webp",
-  "images/IMG-20250726-WA0014.jpg",
-  "images/IMG-20260805-WA0008.jpg",
-  "images/IMG-20260805-WA0009.jpg",
-  "images/IMG-20260805-WA0010.jpg",
-  "images/IMG-20260805-WA0017.jpg",
-  "images/IMG-20260805-WA0020.jpg",
-  "images/IMG-20260805-WA0021.jpg"
+
+"images/1000048755.jpg",
+
+"images/1000053593.webp",
+
+"images/1743531221047.webp",
+
+"images/IMG-20250726-WA0014.jpg",
+
+"images/IMG-20260805-WA0008.jpg",
+
+"images/IMG-20260805-WA0009.jpg",
+
+"images/IMG-20260805-WA0010.jpg",
+
+"images/IMG-20260805-WA0017.jpg",
+
+"images/IMG-20260805-WA0020.jpg",
+
+"images/IMG-20260805-WA0021.jpg"
+
 ];
 
-let current = 0;
+let current=0;
 
-function openGift() {
-  document.querySelector(".opening").style.display = "none";
-  document.getElementById("main").style.display = "block";
+function nextPhoto(){
+
+current++;
+
+if(current>=photos.length){
+
+current=0;
+
 }
 
-setInterval(() => {
-  current = (current + 1) % photos.length;
-  document.getElementById("slide").src = photos[current];
-}, 3000);
+document.getElementById("photo").src=photos[current];
+
+}
