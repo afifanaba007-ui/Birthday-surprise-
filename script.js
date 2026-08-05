b18138a382b27e174f83d1fa605a1668a23eec2a
afifanaba007-ui <afifanaba007@gmail.com>
2026-08-06 01:40:46 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const photos = [
-"images/1000048755.jpg",
-"images/1000053593.webp",
-"images/1743531221047.webp",
-"images/IMG-20250726-WA0014.jpg",
-"images/IMG-20260322-WA0001.jpg",
-"images/IMG-20260805-WA0008.jpg",
-"images/IMG-20260805-WA0009.jpg",
-"images/IMG-20260805-WA0010.jpg",
-"images/IMG-20260805-WA0020.jpg",
-"images/IMG-20260805-WA0021.jpg"
+  "images/1000048755.jpg",
+  "images/1000053593.webp",
+  "images/1743531221047.webp",
+  "images/IMG-20250726-WA0014.jpg",
+  "images/IMG-20260805-WA0008.jpg",
+  "images/IMG-20260805-WA0009.jpg",
+  "images/IMG-20260805-WA0010.jpg",
+  "images/IMG-20260805-WA0017.jpg",
+  "images/IMG-20260805-WA0020.jpg",
+  "images/IMG-20260805-WA0021.jpg"
 ];
 
 let current = 0;
@@ -16,6 +16,9 @@ let current = 0;
 document.getElementById("photo").src = photos[current];
 
 function nextPhoto() {
-  current = (current + 1) % photos.length;
+  current++;
+  if (current >= photos.length) {
+    current = 0;
+  }
   document.getElementById("photo").src = photos[current];
 }
